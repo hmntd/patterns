8dfe738ad58cfafa872dc4c1d63463022eba30b2
hmntd <bazavluk.r.s@gmail.com>
2025-11-14 02:48:40 +0200

    lab3

---
+++ task_1/Interfaces/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patterns_lab_3_1.Interfaces
+{
+    public interface ICalculator
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}
---
+++ task_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using patterns_lab_3_1.Interfaces;
+using patterns_lab_3_1.Models;
+
+ICalculator fullCalc = new FullCalculator();
+ICalculator calculator = new LightCalculator(fullCalc);
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("\nВведіть перше число:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nОберіть дію:");
+        Console.WriteLine("  1. Додавання (+)");
+        Console.WriteLine("  2. Віднімання (-)");
+        Console.WriteLine("  3. Множення (*)");
+        Console.WriteLine("  4. Ділення (/)");
+        Console.WriteLine("  5. Вихід");
+        Console.Write("Ваш вибір (1-5): ");
+
+        string choice = Console.ReadLine();
+        double result = 0;
+
+        switch (choice)
+        {
+            case "1":
+                result = calculator.Add(a, b);
+                break;
+            case "2":
+                result = calculator.Subtract(a, b);
+                break;
+            case "3":
+                result = calculator.Multiply(a, b);
+                break;
+            case "4":
+                result = calculator.Divide(a, b);
+                break;
+            case "5":
+                Console.WriteLine("Завершення роботи. До побачення!");
+                return;
+            default:
+                Console.WriteLine("Невірний вибір. Будь ласка, введіть число від 1 до 5.");
+                continue;
+        }
+
+        Console.WriteLine("-------------------------");
+        Console.WriteLine($"Результат: {result}");
+        Console.WriteLine("-------------------------");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Помилка: Введено невірний числовий формат. Спробуйте ще раз.");
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Помилка: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Помилка: {ex.Message}");
+    }
+}
